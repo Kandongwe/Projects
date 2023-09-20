f32e664bd8a38de02ab9d829980bce7d8a15a386
Collins Kandongwe <kandongwec@berea.edu>
2023-09-19 17:19:24 -0700

    Basic UI set up for app

---
+++ src/Components/Photo.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+export default class Photo  extends Component {
+    render(){
+        const post = this.props.post
+
+        return (
+            <figure className="photo-holder"> 
+
+            </figure>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import Main from './Components/Main';
+import "./styles/stylesheet.css";
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
